a887464ab70f6d64d847d8eafe3dca2f78fcdbcd
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2014-06-05 10:11:24 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Theofanis Pantelides on 6/5/14.
+//  Copyright (c) 2014 theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
